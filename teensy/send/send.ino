00dd50776ca23f79ef5f07cdfc4bf2ded4daa3e4
Cedric Honnet <honnet@telecom-paristech.org>
2013-08-26 14:07:13 -0700

    Teensy: Add debug logs & leds signals.

--- teensy/send/send.ino
+++ teensy/send/send.ino
@@ -10,16 +10,36 @@ int samplebank[NUM_PINS][NUM_SAMPLES];
 int medians[NUM_PINS];
 int minPush = 0;
 int maxPush = 0;
+bool led = 0;
 
 void setup(){
     uart.begin(19200);
     uart.print("Serial is go.");
+    Serial.begin(115200);
+    for (int i=0; i<6; i++, delay(50))
+        digitalWrite(LED_PIN, led = !led);
 
     pinMode(FELT_SUPPLY_PIN, OUTPUT);
+
+    while (0) {
+        digitalWrite(FELT_SUPPLY_PIN, HIGH);
+        int val = analogRead(FELT_ANALOG_PIN);
+        digitalWrite(FELT_SUPPLY_PIN, LOW);
+
+        Serial.print(val);
+        Serial.print("\t");
+        for (int i=0; i<val/8; i++)
+            Serial.print("*");
+        Serial.print("\n");
+
+        digitalWrite(LED_PIN, led = !led);
+        delay(50);
+    }
     calibrate();
 }
 
 void calibrate(){
+    digitalWrite(LED_PIN, LOW);
     const int SAMPLE_PERIOD = 10;  // ms
     const int SAMPLE_NUMBER = 500; // 5 seconds
 
@@ -36,6 +56,8 @@ void calibrate(){
     minPush = 0.85*accumulator/SAMPLE_NUMBER; // 15% security margin
 
     delay(1000);
+    Serial.println("Hit the sensor for 5 sec!");
+    digitalWrite(LED_PIN, HIGH);
 
     // measure the maximum pressure by hitting the felt:
     for (int i=0; i<SAMPLE_NUMBER; i++){
@@ -50,6 +72,11 @@ void calibrate(){
     }
     maxPush *= 1.15; // 15% security margin
 
+    Serial.print("minPush: ");
+    Serial.println(minPush);
+    Serial.print("maxPush: ");
+    Serial.println(maxPush);
+    delay(1000);
 }
 
 int median( int n, int arr[] ){
@@ -76,8 +103,6 @@ int median( int n, int arr[] ){
     }
 }
 
-bool foo = true;
-
 void loop(){
     for (int j=0; j<NUM_SAMPLES; j++) {
         for (int i=0; i<NUM_PINS; i++) {
@@ -95,6 +120,11 @@ void loop(){
         if (i == FELT_ANALOG_PIN) {
             medians[i] = map(medians[i], minPush,maxPush , 0,1023);
             medians[i] = constrain(medians[i], 0,1023);
+            Serial.print(medians[i]);
+            Serial.print("\t |");
+            for (int j=0; j<medians[i]/16; j++)
+                Serial.print("*");
+            Serial.print("\n");
         }
     }
 
@@ -105,9 +135,7 @@ void loop(){
     }
     uart.print(".\r\n");
 
-    digitalWrite(LED_PIN, foo);
-    foo = !foo;
-
+    digitalWrite(LED_PIN, led = !led);
     delay(100); // miliseconds
 }
 
